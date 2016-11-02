6ae795feb675559c0c2d16979aabb37de2ffa75f
Clement <Clement@Clement-PC>
2016-11-02 19:28:29 +0100

    commit user.php

---
+++ model/User.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of User
+ *
+ * @author Clement
+ */
+class User {
+
+    private $nom;
+    private $prenom;
+    private $email;
+    private $motdepasse;
+
+    public function __construct() {
+        
+    }
+
+    public function init($nom, $prenom, $email, $motdepasse) {
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+        $this->email = $email;
+        $this->motdepasse = $motdepasse;
+    }
+
+    public function getNom() {
+        return $this->nom;
+    }
+
+    public function getPrenom() {
+        return $this->prenom;
+    }
+
+}
